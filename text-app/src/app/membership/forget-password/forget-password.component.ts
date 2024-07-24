a5f3435088cc976731b64e96e0bc0e3cc2ab2fe9
Kim Bum Jun <admin@vivabj.com>
2024-07-24 20:31:57 +0900

    Complete forget-password

--- text-app/src/app/membership/forget-password/forget-password.component.ts
+++ text-app/src/app/membership/forget-password/forget-password.component.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { AuthService } from '@app/services/auth.service';
 
 @Component({
   selector: 'app-forget-password',
   standalone: true,
   imports: [
     FormsModule,
+    MatSnackBarModule,
+    MatIconModule
 
   ],
   templateUrl: './forget-password.component.html',
@@ -13,9 +19,45 @@ import { FormsModule } from '@angular/forms';
 })
 export class ForgetPasswordComponent {
 
-  email!: String;
+  email!: string;
+
+  authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+
+  showEmailSent = false;
+  isSubmitting = false;
 
   forgetPassword() {
-
+    this.isSubmitting = true;
+    this.authService.forgetPwd(this.email).subscribe({
+      next: (response) => {
+        if (response.isSuccess) {
+          this.showEmailSent = true
+          this.matSnackBar.open(`${response.message}`, '닫기', {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        } else {
+          this.showEmailSent = false;
+          this.matSnackBar.open(response.message, '닫기', {
+            duration: 5000,
+            horizontalPosition: 'center',
+            verticalPosition: 'top'
+          });
+        }
+      },
+      error: (error: HttpErrorResponse) => {
+        this.showEmailSent = false;
+        this.matSnackBar.open(error.error.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        });
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      }
+    });
   }
 }
--- text-app/src/app/membership/reset-password/reset-password.component.ts
+++ text-app/src/app/membership/reset-password/reset-password.component.ts
@@ -1,12 +1,66 @@
-import { Component } from '@angular/core';
+import { JsonPipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ResetPasswordRequest } from '@app/interfaces/reset-password-request';
+import { AuthService } from '@app/services/auth.service';
 
 @Component({
   selector: 'app-reset-password',
   standalone: true,
-  imports: [],
+  imports: [
+    MatIconModule,
+    FormsModule,
+    JsonPipe
+  ],
   templateUrl: './reset-password.component.html',
   styleUrl: './reset-password.component.scss'
 })
-export class ResetPasswordComponent {
+export class ResetPasswordComponent implements OnInit {
 
+
+  resetPassword = {} as ResetPasswordRequest
+
+  authService = inject(AuthService);
+  matSnackBar = inject(MatSnackBar);
+  router = inject(Router);
+  route = inject(ActivatedRoute);
+
+  isSubmitting = false;
+
+  ngOnInit(): void {
+    this.route.queryParams.subscribe(params => {
+      this.resetPassword.email = params['email'];
+      this.resetPassword.token = params['token'];
+    });
+  }
+
+  resetPasswordHandle() {
+    this.isSubmitting = true;
+    this.authService.resetPassword(this.resetPassword).subscribe({
+      next: (response) => {
+        let ref = this.matSnackBar.open(response.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        });
+
+        ref.onAction().subscribe(() => {
+          this.router.navigate(['/SingIn']);
+        });
+      },
+      error: (error) => {
+        this.matSnackBar.open(error.error.message, 'Close', {
+          duration: 5000,
+          horizontalPosition: 'center',
+          verticalPosition: 'top'
+        });
+      },
+      complete: () => {
+        this.isSubmitting = false;
+      }
+    });
+  }
 }
