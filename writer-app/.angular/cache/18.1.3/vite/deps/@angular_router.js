1cec1c2cf8d71fb9f2577960ec5fdd76faf11ed5
Kim Bum Jun <admin@vivabj.com>
2024-08-02 01:39:03 +0900

    Hide SignUp, SignIn

--- writer-app/.angular/cache/18.1.3/vite/deps/@angular_router.js
+++ writer-app/.angular/cache/18.1.3/vite/deps/@angular_router.js
@@ -1,6 +1,7 @@
 import {
   Title
-} from "./chunk-6EV5YWBZ.js";
+} from "./chunk-52ZX75G4.js";
+import "./chunk-KRXUX56J.js";
 import {
   DOCUMENT,
   HashLocationStrategy,
--- writer-app/src/app/home/home.component.ts
+++ writer-app/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { UtilityService } from '../services/utility.service';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +8,25 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+
+
+  ngOnInit(): void {
+    this.getMyPublicIp();
+  }
+  utilityService = inject(UtilityService);
+
+  myPublicIp = '';
+
+  getMyPublicIp = () => {
+    this.utilityService.getMyPublicIp().subscribe({
+      next: (ip) => {
+        this.myPublicIp = ip;
+      },
+      error: (err) => {
+        this.myPublicIp = err.error.message;
+      }
+    });
+  }
 }
