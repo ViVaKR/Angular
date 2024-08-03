00784f3b54d6754bf12da7df24d057a6b4bdf2e9
Kim Bum Jun <admin@vivabj.com>
2024-08-04 04:49:39 +0900

    Modified role, Validation-error

--- writer-app/README.md
+++ writer-app/README.md
@@ -5,3 +5,27 @@
 ```bash
 try_files $uri $uri/ /index.html;
 ```
+
+## Color
+
+  Pink
+  Fuchsia
+  Purple
+  Violet
+  Indigo
+  Blue
+  Sky
+  Cyan
+  Teal
+  Emerald
+  Green
+  Lime
+  Yellow
+  Amber
+  Orange
+  Red
+  Stone
+  Neutral
+  Zinc
+  Gray
+  Slate
---
+++ writer-app/src/app/services/role.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Role } from '@app/interfaces/role';
+import { RoleCreateRequest } from '@app/interfaces/role-create-request';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoleService {
+
+  apiUrl = "https://localhost:50011";
+
+  http = inject(HttpClient);
+
+  getRoles = (): Observable<Role[]> => this.http.get<Role[]>(`${this.apiUrl}/roles`);
+
+  createRole(role: RoleCreateRequest): Observable<{ message: string }> {
+    return this.http.post<{ message: string }>(`${this.apiUrl}/roles`, role);
+  }
+
+  deleteRole(id: string): Observable<{ message: string }> {
+    return this.http.delete<{ message: string }>(`${this.apiUrl}/roles/${id}`);
+  }
+
+  assignRole(userId: string, roleId: string): Observable<{ message: string }> {
+    return this.http.post<{ message: string }>(`${this.apiUrl}/roles/assign`, { userId, roleId });
+  }
+}
