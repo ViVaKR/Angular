681d4e929ef73fb42a1470afa0b7c5020795dbeb
Kim Bum Jun <admin@vivabj.com>
2025-04-22 09:56:36 +0900

    Re Create buddham-co-kr

--- buddham-co-kr/README.md
+++ buddham-co-kr/README.md
@@ -50,6 +50,8 @@ For end-to-end (e2e) testing, run:
 
 ```bash
 ng e2e
+
+ng add @angular/material
 ```
 
 Angular CLI does not come with an end-to-end testing framework by default. You can choose one that suits your needs.
---
+++ buddham-co-kr/src/app/nav-menu-bar/nav-menu-bar.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-menu-bar',
+  imports: [],
+  templateUrl: './nav-menu-bar.component.html',
+  styleUrl: './nav-menu-bar.component.css'
+})
+export class NavMenuBarComponent {
+
+}
