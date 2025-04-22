fe906c50a814d747a855dfc4a3d2f20b9aacc94e
Kim Bum Jun <admin@vivabj.com>
2025-04-22 09:56:36 +0900

    Re Create buddham-co-kr

--- buddham-co-kr/src/app/app.component.ts
+++ buddham-co-kr/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavMenuBarComponent } from "./nav-menu-bar/nav-menu-bar.component";
+import { FooterBarComponent } from "./footer-bar/footer-bar.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavMenuBarComponent, FooterBarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'buddham-co-kr';
