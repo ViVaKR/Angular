334e5c2089f4281a12011a0e4858a4c3bee48952
Kim Bum Jun <admin@vivabj.com>
2025-07-24 03:32:54 +0900

    Create new buddham for renew

---
+++ buddham/src/app/app.routes.ts
@@ -0,0 +1,9 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+
+export const routes: Routes = [
+    { path: '', redirectTo: 'Home', pathMatch: 'full' },
+    { path: 'Home', component: Home },
+    { path: '**', redirectTo: 'Home' }
+];
