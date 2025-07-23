5a10da0adcb2fdb31128957170d0c67e53a0b127
Kim Bum Jun <admin@vivabj.com>
2025-07-23 23:52:25 +0900

    Modified buddham-co-kr

--- buddham-co-kr/src/app/app.ts
+++ buddham-co-kr/src/app/app.ts
@@ -1,17 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TopMenu } from "./top-menu/top-menu";
-import { Home } from './home/home';
 import { FooterMenu } from "./footer-menu/footer-menu";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TopMenu, Home, FooterMenu],
+  imports: [RouterOutlet, TopMenu, FooterMenu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Buddhist Scriptures');
-
-
 }
