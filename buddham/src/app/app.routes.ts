c7e0d504652a7df863070211f448bb24152a27fe
Kim Bum Jun <admin@vivabj.com>
2025-07-25 15:39:14 +0900

    Modified menu bar, buddham-co-kr

--- buddham/src/app/app.routes.ts
+++ buddham/src/app/app.routes.ts
@@ -1,9 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Texts } from './texts/texts';
+import { Auth } from './services/auth';
+import { Membership } from './membership/membership';
+import { Login } from './membership/login/login';
 
 
 export const routes: Routes = [
     { path: '', redirectTo: 'Home', pathMatch: 'full' },
     { path: 'Home', component: Home },
+    { path: 'Texts', component: Texts },
+    { path: 'Auth', component: Auth },
+    { path: 'Membership', component: Membership },
+    { path: 'Login', component: Login },
     { path: '**', redirectTo: 'Home' }
 ];
