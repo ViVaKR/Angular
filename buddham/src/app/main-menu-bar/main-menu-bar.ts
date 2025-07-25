c2512fa85c07f6ffb05f611f98dd21e81dd1ab6f
Kim Bum Jun <admin@vivabj.com>
2025-07-25 15:39:14 +0900

    Modified menu bar, buddham-co-kr

--- buddham/src/app/main-menu-bar/main-menu-bar.ts
+++ buddham/src/app/main-menu-bar/main-menu-bar.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-menu-bar',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './main-menu-bar.html',
   styleUrl: './main-menu-bar.scss'
 })
 export class MainMenuBar {
 
+  // private router = inject(Router);
+
+  // goTo(url: string[]) {
+  //   this.router.navigate(url);
+  // }
 }
---
+++ buddham/src/app/texts/texts.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-texts',
+  imports: [],
+  templateUrl: './texts.html',
+  styleUrl: './texts.scss'
+})
+export class Texts {
+
+}
