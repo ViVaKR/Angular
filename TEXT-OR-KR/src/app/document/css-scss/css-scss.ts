090963440856502532bd278083fe5441e46ae483
Kim Bum Jun <admin@vivabj.com>
2025-09-29 22:42:48 +0900

    Renamed CODE-TEXT-OR-KR to TEXT-OR-KR

---
+++ TEXT-OR-KR/src/app/document/css-scss/css-scss.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-css-scss',
+  imports: [],
+  templateUrl: './css-scss.html',
+  styleUrl: './css-scss.scss'
+})
+export class CssScss {
+
+}
