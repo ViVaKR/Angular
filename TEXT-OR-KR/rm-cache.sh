b99f049fdac44f4e97dc04a92208d7f5ba2102b7
Kim Bum Jun <admin@vivabj.com>
2025-10-02 13:07:44 +0900

    Modified TEXT-OR-KR, demo

---
+++ TEXT-OR-KR/rm-cache.sh
@@ -0,0 +1,7 @@
+#! /usr/bin/env zsh
+
+rm -rf node_modules
+rm -rf package-lock.json
+rm -rf dist
+rm -rf .angular/cache
+
--- TEXT-OR-KR/src/app/app.config.ts
+++ TEXT-OR-KR/src/app/app.config.ts
@@ -19,5 +19,6 @@ export const appConfig: ApplicationConfig = {
     ),
     provideHttpClient(withFetch()),
     provideClientHydration(withEventReplay()),
+
   ]
 };
